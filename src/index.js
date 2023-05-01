e0b66b162f1b6ceb1038aa685abc66aec123fcca
lllastun <lllastun@gmail.com>
2023-05-01 07:47:16 +0300

    start

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import './css/styles.css';
-import fetchCountries from './fetchCountries.js';
+import {fetchCountries} from './fetchCountries.js';
+import { Notify } from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
+
 fetchCountries();
